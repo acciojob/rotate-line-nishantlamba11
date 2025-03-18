40469945c2e3e4e5e5be304872bc79bc7c0033aa
Nishant lamba <131985675+nishantlamba11@users.noreply.github.com>
2025-03-18 16:01:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+
+let angle = 0;
+
+
+function rotateLine() {
+ 
+  const line = document.getElementById('line');
+
+ 
+  angle += 2;
+
+
+  line.style.transform = `rotate(${angle}deg)`;
+
+  
+  if (angle >= 360) {
+    angle -= 360;
+  }
+}
+
+
+setInterval(rotateLine, 20);
